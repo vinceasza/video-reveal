919f50b520274ce65622119184e05331da2422a7
Vincent Szajdzicki <vinszajdzicki@gmail.com>
2015-05-28 22:33:47 +0100

    Modular version

---
+++ src/js/js.js
@@ -0,0 +1,64 @@
+var navi = (function(){
+  var $window = $(window);
+  var $navUl = $('.navigation ul');
+
+  return {
+    toggle: function() {
+
+        $window.bind('load resize',function(){
+          if ($window.width() < 850){
+            $navUl.hide();
+            $('.responsive-menu-btn').on('click', function(e) {
+              e.preventDefault();
+              $navUl.toggle();
+            });
+          } else {
+            $navUl.show();
+          }
+        });
+    },
+    scrollToDiv: function(){
+      $navUl.on('click','a',function(e){
+        e.preventDefault();
+        var hash = $(this).attr('href');
+        var pos = $(hash).offset();
+        $('html, body').animate({scrollTop: pos.top}, "slow");
+      });
+    },
+    highlighter: function(hash,top, viewportTop){
+      if ( viewportTop > top - 200 ) {
+        $(".navigation ul li a").each(function(index){
+          if (this.hash == hash){
+            $(this).addClass('selected');
+          } else {
+            $(this).removeClass('selected');
+          }
+        });
+      }
+    },
+    scrollNav: function(){
+      $window.bind('scroll load', function(){
+        var viewportTop = $window.scrollTop();
+        var winHeight = $(document).innerHeight();
+        var a  = $('.navigation ul li a');
+        for (var i=0; i < a.length; i++){
+          var currentAnchor = a[i];
+          var nextAnchor = a[i];
+          var hash = $(currentAnchor).attr('href');
+          var nextHash = $(nextAnchor).attr('href');
+          var top = $(hash).offset().top;
+          navi.highlighter(hash,top,viewportTop);
+        }
+      });
+    }
+  }
+}());
+
+(function(){
+
+
+navi.toggle();
+navi.scrollToDiv();
+navi.scrollNav();
+
+}());
